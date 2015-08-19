fccee09656f9f3a1d26ae472bdadd4635cf4cd53
AvanthiManda <avanthimanda4@gmail.com>
2015-08-18 19:08:32 -0700

    First Commit

---
+++ Aug2015_practice/OddEvenPrime.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aug2015_practice
+{
+   public class OddEvenPrime
+    {
+       /// <summary>
+       /// to print odd,even and prime using switch case
+       /// </summary>
+       /// <param name="args"></param>
+       /* static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a program to compute 1. EVEN  2.ODD 3.PRIME");
+            int input = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Please enter a number");
+            int num = Convert.ToInt16(Console.ReadLine());
+            OddEvenPrime prog = new OddEvenPrime();
+            bool output = false ;
+
+            switch (input)
+            {
+                case 1:
+                    output = prog.isEven(num);
+                    break;
+                case 2:
+                    output = prog.isOdd(num);
+                    break;
+                case 3:
+                    output = prog.isPrime(num);
+                    break;
+                default:
+                    break;
+            }            
+            Console.WriteLine(output);
+            Console.ReadLine();
+        } */
+        public bool isEven(int num)
+        {
+            bool result = false;
+            if (num % 2 == 0)
+            {
+                result = true;
+            }
+            return result;
+        }
+        public bool isOdd(int num)
+        {
+            bool result = false;
+            if (num % 2 != 0)
+            {
+                result = true;
+            }
+            return result;
+        }
+        public bool isPrime(int num)
+        {
+            bool result = true;
+            for(int i =2; i * i <= num ; i++)
+            {
+                if (num % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Aug2015_practice/Reverse_Palindrome_fibonacii.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aug2015_practice
+{
+    public class Reverse_Palindrome_fibonacii
+    {
+        public static void Main()
+        {
+            Reverse_Palindrome_fibonacii prog = new Reverse_Palindrome_fibonacii();
+            string print = prog.reverseString("Avanthi");
+            Console.WriteLine(print);
+            prog.fizzBuzz();
+            Console.ReadLine();           
+        }
+        public string reverseString(string name)
+        {
+            string output = "";
+            for (int i = name.Length -1 ; i >= 0; i--)
+            {
+                output = output + name[i];
+            }
+            return output;
+        }
+        public int fizzBuzz()
+        {
+            int output = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FIZZBUZZ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("FIZZ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("BUZZ");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            return output;
+        }
+    }
+
+}
